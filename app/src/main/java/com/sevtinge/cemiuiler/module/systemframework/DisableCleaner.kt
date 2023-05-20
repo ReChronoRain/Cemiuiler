5a9decbb51f874f76cd4e3e8b0fe2a9a870f8f59
nakixii <61456408+nakixii@users.noreply.github.com>
2023-05-20 14:03:08 +0800

    修复 系统框架-其他-禁止杀后台应用

--- app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/DisableCleaner.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/systemframework/DisableCleaner.kt
@@ -4,52 +4,49 @@ import com.sevtinge.cemiuiler.module.base.BaseHook
 
 object DisableCleaner : BaseHook() {
     override fun init() {
-        findAndHookMethod("com.android.server.am.ActivityManagerService",
-            "checkExcessivePowerUsage",
+        hookAllMethod("com.android.server.am.ActivityManagerService", "checkExcessivePowerUsage",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = null
                 }
             }
         )
-        findAndHookMethod("com.android.server.am.OomAdjuster", "shouldKillExcessiveProcesses",
+        hookAllMethod("com.android.server.am.OomAdjuster", "shouldKillExcessiveProcesses",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = false
                 }
             }
         )
-        findAndHookMethod("com.android.server.am.OomAdjuster", "updateAndTrimProcessLSP",
+        hookAllMethod("com.android.server.am.OomAdjuster", "updateAndTrimProcessLSP",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.args?.set(2, 0)
                 }
             }
         )
-        findAndHookMethod("com.android.server.am.PhantomProcessList",
-            "trimPhantomProcessesIfNecessary",
+        hookAllMethod("com.android.server.am.PhantomProcessList", "trimPhantomProcessesIfNecessary",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = null
                 }
             }
         )
-        findAndHookMethod("com.android.server.am.ProcessMemoryCleaner",
-            "checkBackgroundProcCompact",
+        hookAllMethod("com.android.server.am.ProcessMemoryCleaner", "checkBackgroundProcCompact",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = null
                 }
             }
         )
-        findAndHookMethod("com.android.server.am.SystemPressureController", "nStartPressureMonitor",
+        hookAllMethod("com.android.server.am.SystemPressureController", "nStartPressureMonitor",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = null
                 }
             }
         )
-        findAndHookMethod("com.android.server.wm.RecentTasks", "trimInactiveRecentTasks",
+        hookAllMethod("com.android.server.wm.RecentTasks", "trimInactiveRecentTasks",
             object : MethodHook() {
                 override fun before(param: MethodHookParam?) {
                     param?.result = null
