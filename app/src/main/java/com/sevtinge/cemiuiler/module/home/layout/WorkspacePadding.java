f579f7dd3db6f7ff027794ea8981f2829b920b5a
mingjia.ge <gemingjia0201@163.com>
2023-07-19 17:02:18 +0800

    修复 系统桌面-布局-主屏幕-顶部间距 在RELEASE-4.39.9.6586版本桌面失效

--- app/src/main/java/com/sevtinge/cemiuiler/module/home/layout/WorkspacePadding.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/home/layout/WorkspacePadding.java
@@ -1,5 +1,7 @@
 package com.sevtinge.cemiuiler.module.home.layout;
 
+import static com.sevtinge.cemiuiler.utils.Helpers.getPackageVersionCode;
+
 import android.content.Context;
 
 import com.sevtinge.cemiuiler.module.base.BaseHook;
@@ -10,10 +12,14 @@ public class WorkspacePadding extends BaseHook {
     Context mContext;
     Class<?> mDeviceConfig;
 
+    static int mHomeVersionCode;
+    final static int NEW_HOME_WORKSPACE_PADDING_TOP_VERSION = 439096586;
+
     @Override
     public void init() {
 
         mDeviceConfig = findClassIfExists("com.miui.home.launcher.DeviceConfig");
+        mHomeVersionCode = getPackageVersionCode(lpparam);
 
         findAndHookMethod(mDeviceConfig, "Init", Context.class, boolean.class, new MethodHook() {
             @Override
@@ -32,12 +38,21 @@ public class WorkspacePadding extends BaseHook {
         }
 
         if (mPrefsMap.getBoolean("home_layout_workspace_padding_top_enable")) {
-            findAndHookMethod(mDeviceConfig, "getWorkspaceCellPaddingTop", new MethodHook() {
-                @Override
-                protected void before(MethodHookParam param) {
-                    param.setResult(DisplayUtils.dip2px(mContext, mPrefsMap.getInt("home_layout_workspace_padding_top", 0)));
-                }
-            });
+            if (mHomeVersionCode >= NEW_HOME_WORKSPACE_PADDING_TOP_VERSION) {
+                findAndHookMethod(mDeviceConfig, "getWorkspaceCellPaddingTop", Context.class, new MethodHook() {
+                    @Override
+                    protected void before(MethodHookParam param) {
+                        param.setResult(DisplayUtils.dip2px(mContext, mPrefsMap.getInt("home_layout_workspace_padding_top", 0)));
+                    }
+                });
+            } else {
+                findAndHookMethod(mDeviceConfig, "getWorkspaceCellPaddingTop", new MethodHook() {
+                    @Override
+                    protected void before(MethodHookParam param) {
+                        param.setResult(DisplayUtils.dip2px(mContext, mPrefsMap.getInt("home_layout_workspace_padding_top", 0)));
+                    }
+                });
+            }
         }
     }
 }
