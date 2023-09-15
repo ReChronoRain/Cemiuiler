5bf7935d9193a8481ae6abedec5d028e037c02e2
HChenX <xhc-xh@qq.com>
2023-09-15 18:18:57 +0800

    [新增] 桌面-其他-禁止提示"请再划一次" Toast
    Signed-off-by: HChenX <xhc-xh@qq.com>

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/home/ToastSlideAgain.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/home/ToastSlideAgain.java
@@ -9,39 +9,21 @@ import de.robv.android.xposed.XposedHelpers;
 
 public class ToastSlideAgain extends BaseHook {
     public XC_MethodHook.Unhook unhook = null;
-    public XC_MethodHook.Unhook unhookA = null;
 
     @Override
     public void init() {
         findAndHookMethod("com.miui.home.recents.NavStubView",
-            "onPointerEvent",
-            MotionEvent.class,
+            "onPointerEvent", MotionEvent.class,
             new MethodHook() {
                 @Override
-                protected void before(MethodHookParam param) throws Throwable {
-                    super.before(param);
-                    unhook =
-                        XposedHelpers.findAndHookMethod(findClassIfExists("android.widget.Toast"),
-                            "show", new XC_MethodHook() {
-                                @Override
-                                protected void beforeHookedMethod(MethodHookParam param) {
-                                    param.setResult(null);
-                                    logI("im hook Toast show");
-                                }
-                            }
-                        );
+                protected void before(MethodHookParam param) {
+                    unhook = hookToast();
                     logI("im hook onPointerEvent");
                 }
 
                 @Override
-                protected void after(MethodHookParam param) throws Throwable {
-                    super.after(param);
-                    if (unhook != null) {
-                        unhook.unhook();
-                        logI("the unhook is: " + unhook);
-                    } else {
-                        logE("the unhook is: null");
-                    }
+                protected void after(MethodHookParam param) {
+                    unHook(unhook);
                 }
             }
         );
@@ -50,29 +32,36 @@ public class ToastSlideAgain extends BaseHook {
             "onStartGesture", new MethodHook() {
                 @Override
                 protected void before(MethodHookParam param) {
-                    unhookA =
-                        XposedHelpers.findAndHookMethod(findClassIfExists("android.widget.Toast"),
-                            "show", new XC_MethodHook() {
-                                @Override
-                                protected void beforeHookedMethod(MethodHookParam param) {
-                                    param.setResult(null);
-                                    logI("im hook Toast show");
-                                }
-                            }
-                        );
+                    unhook = hookToast();
                     logI("im hook onStartGesture");
                 }
 
                 @Override
                 protected void after(MethodHookParam param) {
-                    if (unhookA != null) {
-                        unhookA.unhook();
-                        logI("the unhook is: " + unhookA);
-                    } else {
-                        logE("the unhook is: null");
-                    }
+                    unHook(unhook);
                 }
             }
         );
+    }
+
+    public XC_MethodHook.Unhook hookToast() {
+        return XposedHelpers.findAndHookMethod(findClassIfExists("android.widget.Toast"),
+            "show", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    param.setResult(null);
+                    logI("im hook Toast show");
+                }
+            }
+        );
+    }
+
+    public void unHook(XC_MethodHook.Unhook unhook) {
+        if (unhook != null) {
+            unhook.unhook();
+            logI("the unhook is: " + unhook);
+        } else {
+            logE("the unhook is: null");
+        }
     }
 }
