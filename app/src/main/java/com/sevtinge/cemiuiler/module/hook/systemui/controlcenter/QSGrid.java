33c1c53352b162c651746076580572831f4fdd7b
ZYM <zym013579@users.noreply.github.com>
2023-09-22 23:35:33 +0800

    Update QSGrid.java

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QSGrid.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QSGrid.java
@@ -15,52 +15,37 @@ public class QSGrid extends BaseHook {
 
     @Override
     public void init() {
-        int cols = mPrefsMap.getInt("system_control_center_old_qs_columns", 2);
-        int rows = mPrefsMap.getInt("system_control_center_old_qs_rows", 1);
-        int colsRes = R.integer.quick_settings_num_columns_3;
-        int rowsRes = R.integer.quick_settings_num_rows_4;
-
-        switch (cols) {
-            case 3 -> colsRes = R.integer.quick_settings_num_columns_3;
-            case 4 -> colsRes = R.integer.quick_settings_num_columns_4;
-            case 5 -> colsRes = R.integer.quick_settings_num_columns_5;
-            case 6 -> colsRes = R.integer.quick_settings_num_columns_6;
-            case 7 -> colsRes = R.integer.quick_settings_num_columns_7;
-        }
-
-        switch (rows) {
-            case 2 -> rowsRes = R.integer.quick_settings_num_rows_2;
-            case 3 -> rowsRes = R.integer.quick_settings_num_rows_3;
-            case 4 -> rowsRes = R.integer.quick_settings_num_rows_4;
-            case 5 -> rowsRes = R.integer.quick_settings_num_rows_5;
-        }
-
-        if (cols > 2)
-            mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_columns", colsRes);
-        if (rows > 1)
-            mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_rows", rowsRes);
-        int test = ((ViewGroup) this.thisObject).getResources().getConfiguration().orientation;
-
         Helpers.hookAllMethods("com.android.systemui.qs.MiuiTileLayout", lpparam.classLoader, "addTile", new MethodHook() {
             @Override
-            protected void before(MethodHookParam param) {
-                int orientation = ((ViewGroup) param.thisObject).getResources().getConfiguration().orientation;
-                
+            protected void after(MethodHookParam param) {
+                int cols = mPrefsMap.getInt("system_control_center_old_qs_columns", 2);
                 int rows = mPrefsMap.getInt("system_control_center_old_qs_rows", 1);
+                int colsRes = R.integer.quick_settings_num_columns_3;
                 int rowsRes = R.integer.quick_settings_num_rows_4;
+
+                switch (cols) {
+                    case 3 -> colsRes = R.integer.quick_settings_num_columns_3;
+                    case 4 -> colsRes = R.integer.quick_settings_num_columns_4;
+                    case 5 -> colsRes = R.integer.quick_settings_num_columns_5;
+                    case 6 -> colsRes = R.integer.quick_settings_num_columns_6;
+                    case 7 -> colsRes = R.integer.quick_settings_num_columns_7;
+                }
+
                 switch (rows) {
                     case 2 -> rowsRes = R.integer.quick_settings_num_rows_2;
                     case 3 -> rowsRes = R.integer.quick_settings_num_rows_3;
                     case 4 -> rowsRes = R.integer.quick_settings_num_rows_4;
                     case 5 -> rowsRes = R.integer.quick_settings_num_rows_5;
                 }
-                
-                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_rows", rowsRes);
-                } else {
-                    mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_rows", R.integer.quick_settings_num_rows_2);
-                }
 
+                if (cols > 2)
+                    mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_columns", colsRes);
+                int orientation = ((ViewGroup) param.thisObject).getResources().getConfiguration().orientation;
+                if (rows > 1 && orientation == Configuration.ORIENTATION_LANDSCAPE)
+                    mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_rows", rowsRes);
+                if (rows > 1 && orientation == Configuration.ORIENTATION_PORTRAIT)
+                    mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_num_rows", R.integer.quick_settings_num_rows_2);
+                //int test = ((ViewGroup) this.thisObject).getResources().getConfiguration().orientation;
                 //updateLayout(param.args[0], XposedHelpers.getIntField(param.thisObject, "mRows"), ((ViewGroup) param.thisObject).getResources().getConfiguration().orientation);
             }
         });
