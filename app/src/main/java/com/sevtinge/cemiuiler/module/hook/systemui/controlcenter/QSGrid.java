75f2d44797ee5119798febe0c51dc79c6c05cf7b
ZYM <zym013579@users.noreply.github.com>
2023-09-22 12:56:54 +0800

    Update QSGrid.java

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QSGrid.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QSGrid.java
@@ -43,7 +43,7 @@ public class QSGrid extends BaseHook {
 
         Helpers.hookAllMethods("com.android.systemui.qs.MiuiTileLayout", lpparam.classLoader, "addTile", new MethodHook() {
             @Override
-            protected void after(MethodHookParam param) {
+            protected void before(MethodHookParam param) {
                 int orientation = ((ViewGroup) param.thisObject).getResources().getConfiguration().orientation;
                 if (orientation == Configuration.ORIENTATION_PORTRAIT) {
                     mResHook.setObjectReplacement("com.android.systemui", "integer", "quick_settings_num_rows", 5);
