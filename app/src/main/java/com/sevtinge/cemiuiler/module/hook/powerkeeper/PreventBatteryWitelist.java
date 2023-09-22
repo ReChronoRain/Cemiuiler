5d6bc3435134a0765c0da9beac695bae40ff69c0
ZYM <zym013579@users.noreply.github.com>
2023-09-22 21:38:54 +0800

    Update PreventBatteryWitelist.java

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/powerkeeper/PreventBatteryWitelist.java
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/powerkeeper/PreventBatteryWitelist.java
@@ -6,7 +6,7 @@ import io.github.libxposed.api.XposedInterface;
 public class PreventBatteryWitelist extends BaseHook {
     @Override
     public void init() {
-        Helpers.hookAllMethods("com.miui.powerkeeper.utils.CommonAdapter", lpparam.getClassLoader(), "addPowerSaveWhitelistApps", new MethodHook(20000) {
+        Helpers.hookAllMethods("com.miui.powerkeeper.utils.CommonAdapter", lpparam.classLoader, "addPowerSaveWhitelistApps", new MethodHook(20000) {
             @Override
             protected void before(MethodHookParam param) throws Throwable {
                 param.setResult(null);
