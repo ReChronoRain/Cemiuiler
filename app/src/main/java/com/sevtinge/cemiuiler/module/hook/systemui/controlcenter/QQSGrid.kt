9bf3548546865dc4ed462b3318813667d274971e
zym013579 <zym013579@gmail.com>
2023-09-24 17:35:03 +0800

    test

--- app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QQSGrid.kt
+++ app/src/main/java/com/sevtinge/cemiuiler/module/hook/systemui/controlcenter/QQSGrid.kt
@@ -4,6 +4,10 @@ import android.content.res.Configuration
 import android.view.View
 import android.view.ViewGroup
 
+import com.github.kyuubiran.ezxhelper.ClassUtils.loadClass
+import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createHook
+import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
+
 import com.sevtinge.cemiuiler.R
 import com.sevtinge.cemiuiler.module.base.BaseHook
 import com.sevtinge.cemiuiler.utils.Helpers
@@ -15,6 +19,14 @@ class QQSGrid : BaseHook() {
         val cols = mPrefsMap.getInt("system_control_center_old_qs_grid_columns", 5);
         //val colsHorizontal = mPrefsMap.getInt("system_control_center_old_qs_grid_columns_horizontal", 6);
 
+        loadClass("com.android.systemui.qs.MiuiQuickQSPanel").methodFinder().first {
+                name == "setMaxTiles" && parameterCount == 1
+            }.createHook {
+                before {
+                    it.args[0] = cols
+                }
+            }
+/*
         Helpers.findAndHookMethod(
             "com.android.systemui.qs.MiuiQuickQSPanel",
             lpparam.classLoader,
@@ -32,6 +44,7 @@ class QQSGrid : BaseHook() {
                 }
             }
         )
+*/
         //mResHook.setResReplacement("com.android.systemui", "integer", "quick_settings_qqs_count", colsResId);
     }
 }
