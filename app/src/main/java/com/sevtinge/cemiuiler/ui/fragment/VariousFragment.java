70b4d17fa73f14895bc60eaab2b56a45818fbcbe
HChenX <xhc-xh@qq.com>
2023-09-25 20:11:56 +0800

    [新增] 其他-禁止显示正在使用流量加速Toast
    Signed-off-by: HChenX <xhc-xh@qq.com>

--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/VariousFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/VariousFragment.java
@@ -5,9 +5,13 @@ import static com.sevtinge.cemiuiler.utils.devicesdk.SystemSDKKt.isAndroidT;
 import static com.sevtinge.cemiuiler.utils.devicesdk.SystemSDKKt.isMoreAndroidVersion;
 import static com.sevtinge.cemiuiler.utils.devicesdk.SystemSDKKt.isMoreMiuiVersion;
 
+import android.content.Context;
+
 import com.sevtinge.cemiuiler.R;
 import com.sevtinge.cemiuiler.ui.fragment.base.SettingsPreferenceFragment;
+import com.sevtinge.cemiuiler.utils.ALPermissionManager;
 
+import moralnorm.appcompat.app.AlertDialog;
 import moralnorm.preference.Preference;
 import moralnorm.preference.SwitchPreference;
 
@@ -34,5 +38,24 @@ public class VariousFragment extends SettingsPreferenceFragment {
 
         mDisableBluetoothRestrict.setOnPreferenceChangeListener((preference, o) -> true);
         mDisableDeviceLog.setOnPreferenceChangeListener((preference, o) -> true);
+
+        SwitchPreference linkTurboToast = findPreference("prefs_key_various_disable_link_turbo_toast");
+        linkTurboToast.setOnPreferenceChangeListener((preference, o) -> {
+            alertDialog();
+            return true;
+        });
     }
+
+    public void alertDialog() {
+        Context context = getActivity();
+        new AlertDialog.Builder(context)
+            .setCancelable(false)
+            .setTitle(getResources().getString(R.string.soft_reboot) + " " + getResources().getString(R.string.reboot_networkboost))
+            .setMessage(getResources().getString(R.string.restart_app_desc, " " + getResources().getString(R.string.reboot_networkboost) + " "))
+            .setHapticFeedbackEnabled(true)
+            .setPositiveButton(android.R.string.ok, (dialog, which) -> ALPermissionManager.RootCommand("killall com.xiaomi.NetworkBoost"))
+            .setNegativeButton(android.R.string.cancel, null)
+            .show();
+    }
+
 }
