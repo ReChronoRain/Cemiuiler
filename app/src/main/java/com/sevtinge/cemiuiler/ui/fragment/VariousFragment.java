51d60ba295f0dff118ee33405d38dd204e0eb766
HChenX <xhc-xh@qq.com>
2023-09-28 23:30:30 +0800

    [新增] 其他-禁止显示正在使用流量加速Toast [完善] 阳光模式 被催惨了X_X
    Signed-off-by: HChenX <xhc-xh@qq.com>

--- app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/VariousFragment.java
+++ app/src/main/java/com/sevtinge/cemiuiler/ui/fragment/VariousFragment.java
@@ -38,24 +38,5 @@ public class VariousFragment extends SettingsPreferenceFragment {
 
         mDisableBluetoothRestrict.setOnPreferenceChangeListener((preference, o) -> true);
         mDisableDeviceLog.setOnPreferenceChangeListener((preference, o) -> true);
-
-        SwitchPreference linkTurboToast = findPreference("prefs_key_various_disable_link_turbo_toast");
-        linkTurboToast.setOnPreferenceChangeListener((preference, o) -> {
-            alertDialog();
-            return true;
-        });
     }
-
-    public void alertDialog() {
-        Context context = getActivity();
-        new AlertDialog.Builder(context)
-            .setCancelable(false)
-            .setTitle(getResources().getString(R.string.soft_reboot) + " " + getResources().getString(R.string.reboot_networkboost))
-            .setMessage(getResources().getString(R.string.restart_app_desc, " " + getResources().getString(R.string.reboot_networkboost) + " "))
-            .setHapticFeedbackEnabled(true)
-            .setPositiveButton(android.R.string.ok, (dialog, which) -> ALPermissionManager.RootCommand("killall com.xiaomi.NetworkBoost"))
-            .setNegativeButton(android.R.string.cancel, null)
-            .show();
-    }
-
 }
